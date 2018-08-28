6bb424f875e3cecd1089a8d00d114e007ddd4f50
helenosheaa <helenosheaa@gmail.com>
2018-08-28 15:45:15 +0100

    initial commit

---
+++ src/containers/SongContainer.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import SongList from '../components/SongList.js'
+
+class SongContainer extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      songs: []
+    };
+  }
+
+  componentDidMount(){
+    const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((songs) => {
+        this.setState({ songs: songs })
+      });
+  }
+
+  render(){
+    return (
+      <div className="song-container">
+        <h2>Top 20</h2>
+        <SongList songs={this.state.songs}/>
+      </div>
+    );
+  }
+}
